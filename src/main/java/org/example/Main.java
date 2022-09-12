0d1bcc6746cacca1279d2aad93e7c2ef4126cd11
ndrtprv <ndrtprv@gmail.com>
2022-09-12 20:18:08 +0300

    Changes in Main class.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Employee emp1 = new Employee("Vlad Bezruk");
+        Employee emp2 = new Employee("Andrii Toporov", 2400);
         emp1.printEmployee();
+        emp2.printEmployee();
     }
 }
